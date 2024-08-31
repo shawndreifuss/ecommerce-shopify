2cc5528895b384a687e256f2c10617c9272c40a0
Shawn Dreifuss <shawndreifuss@icloud.com>
2024-08-30 18:57:39 -0700

    login rout created

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthLayout = ({ children }: any) => {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default AuthLayout
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-   <div className="semi-bold">
-hello
-
-   </div>
+    <main className="flex h-full flex-col items-center justify-center gap-6 bg-sky-900">
+      <div className="space-y-6 text-center flex flex-col">
+        <h1 className="text-6xl font-semi-bold text-white drop-shadow-xl">
+          🔐 Auth
+        </h1>
+        <p className="text-lg text-white">
+          A simple Auth application service
+        </p>
+        <LoginButton>
+          <Button variant="secondary" size="lg">
+            Sign in
+          </Button>
+        </LoginButton>
+      </div>
+    </main>
   );
 }
